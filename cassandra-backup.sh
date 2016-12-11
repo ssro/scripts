3ac530719792a37335068ab1901844b6ab686862
Sebastian <ssro@users.noreply.github.com>
2016-12-11 23:28:36 +0700

    Grouped vars & using aws sync

--- cassandra-backup.sh
+++ cassandra-backup.sh
@@ -18,21 +18,23 @@
 
 set -e
 
-export AWS_DEFAULT_REGION='' # us-east-1
-export AWS_ACCESS_KEY_ID='' # AKIAI44QH8DHBEXAMPLE
-export AWS_SECRET_ACCESS_KEY='' # je7MtGbClwBF/2Zp9Utk/h3yCo8nvbEXAMPLEKEY
+export AWS_DEFAULT_REGION="" # us-east-1
+export AWS_ACCESS_KEY_ID="" # AKIAI44QH8DHBEXAMPLE
+export AWS_SECRET_ACCESS_KEY="" # je7MtGbClwBF/2Zp9Utk/h3yCo8nvbEXAMPLEKEY
 
-# Create the backup directory & define vars
-mkdir -p /mnt/backup/cassandra
-_BACKUP_DIR="" # "/mnt/backup/cassandra"
-chown -R `whoami` ${_BACKUP_DIR} && chmod -R 775 ${_BACKUP_DIR}
 _S3_BUCKET="" # "s3://yourbucket/"
-
-# Define the keyspace and snapshot directory
 _KEYSPACE="" # "mykeyspace"
 _SNAP_DIR="" # "/mnt/cassandra/data/${_KEYSPACE}/*/snapshots/"
-
 _NODETOOL=`which nodetool`
+
+# Create the backup directory & define it as a var
+mkdir -p $HOME/backup/cassandra
+_BACKUP_DIR="${HOME}/backup/cassandra" # "/backup/cassandra"
+
+# Change ownership (can choose other user than the user running the script)
+chown -R `whoami` ${_BACKUP_DIR} && chmod -R 775 ${_BACKUP_DIR}
+
+# Take the snapshot of the desired keyspace
 ${_NODETOOL} snapshot -t ${_KEYSPACE}-$(hostname)-$(date +%F) ${_KEYSPACE}
 wait
 
@@ -40,8 +42,8 @@ wait
 tar cvzf ${_BACKUP_DIR}/$(hostname)-$(date +%F).tar.gz ${_SNAP_DIR}
 
 # Upload to S3 (adapt the script to whatever you're using, eg. FTP, RSYNC, SCP...)
-# You can use a bandwidth limiter like trickle in case you have limited bandwidth
-aws s3 cp ${_BACKUP_DIR}/$(hostname)-$(date +%F).tar.gz ${_S3_BUCKET}
+# You can use a bandwidth limiter like trickle in case you have limited bandwidth and/or slow network
+aws s3 sync ${_BACKUP_DIR}/$(hostname)-$(date +%F).tar.gz ${_S3_BUCKET}
 wait
 
 # Cleanup
