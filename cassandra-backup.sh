fd1c41b69728d632dbac68e646d725613c64a135
Sebastian <ssro@users.noreply.github.com>
2016-12-12 00:12:05 +0700

    Including schema backup; sync everything

--- cassandra-backup.sh
+++ cassandra-backup.sh
@@ -34,6 +34,12 @@ _BACKUP_DIR="${HOME}/backup/cassandra" # "/backup/cassandra"
 # Change ownership (can choose other user than the user running the script)
 chown -R `whoami` ${_BACKUP_DIR} && chmod -R 775 ${_BACKUP_DIR}
 
+# Backup entire database schema
+cqlsh -h `hostname` -e "DESC SCHEMA" > ${_BACKUP_DIR}/db_schema-$(date +%F).cql
+
+# Alternatively you can backup a keyspace schema
+# cqlsh -e "DESC KEYSPACE keyspace" > ${_BACKUP_DIR}/keyspace_schema.cql
+
 # Take the snapshot of the desired keyspace
 ${_NODETOOL} snapshot -t ${_KEYSPACE}-$(hostname)-$(date +%F) ${_KEYSPACE}
 wait
@@ -43,11 +49,11 @@ tar cvzf ${_BACKUP_DIR}/$(hostname)-$(date +%F).tar.gz ${_SNAP_DIR}
 
 # Upload to S3 (adapt the script to whatever you're using, eg. FTP, RSYNC, SCP...)
 # You can use a bandwidth limiter like trickle in case you have limited bandwidth and/or slow network
-aws s3 sync ${_BACKUP_DIR}/$(hostname)-$(date +%F).tar.gz ${_S3_BUCKET}
+aws s3 sync --no-guess-mime-type ${_BACKUP_DIR}/. ${_S3_BUCKET}
 wait
 
 # Cleanup
-${_NODETOOL} clearsnapshot && rm -rf ${_BACKUP_DIR}/$(hostname)-$(date +%F).tar.gz
+${_NODETOOL} clearsnapshot && rm -rf ${_BACKUP_DIR}/*
 
 
 unset AWS_DEFAULT_REGION
