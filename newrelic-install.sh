7527c4a285743d7eec8a4edf754c376fd815809c
Sebastian Sasu <sebastian@nologin.ro>
2017-06-25 23:47:09 +0700

    Initial commit

---
+++ newrelic-install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Install NewRelic agent on Centos 6, 7 - 64-bit
+# Optional - enable docker monitoring
+
+# Author Sebastian Sasu <sebi@nologin.ro>
+
+sudo yum -y install https://yum.newrelic.com/pub/newrelic/el5/x86_64/newrelic-repo-5-3.noarch.rpm \
+  && yum -y install newrelic-sysmond
+
+echo ""
+echo "Enter your license key: "
+read license
+
+sudo nrsysmond-config --set license_key="$license"
+
+echo "Starting & enabling newrelic monitor...."
+sudo /etc/init.d/newrelic-sysmond start
+sudo /sbin/chkconfig newrelic-sysmond on
+
+# Enable docker
+# sudo groupadd -r docker
+echo ""
+echo "Enable docker monitoring? y/n"
+read answer
+
+if [[ "$answer" != "y" ]]; then
+  exit 0
+else sudo usermod -a -G docker newrelic && sudo /etc/init.d/newrelic-sysmond restart
+fi
