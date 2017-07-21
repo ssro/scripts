52935d0f8be1dcc0c65439742fcdb10a8aa8ea36
Sebastian <ssro@users.noreply.github.com>
2017-07-21 14:14:48 +0700

    Update add_swap.sh

--- add_swap.sh
+++ add_swap.sh
@@ -1,12 +1,39 @@
 #!/bin/bash
 
-# Create swap space on a CentOS7 EC2 machine (or bare metal)
-# Best suited for XFS filesystem
-# Swap file size is 4G
+# Add 4G of swapfile on CentOS or Debian systems
 
-sudo dd if=/dev/zero of=/swapfile count=4096 bs=1MiB
-sudo chmod 600 /swapfile
-sudo mkswap /swapfile
-sudo swapon /swapfile
+
+centos_swap() {
+  sudo dd if=/dev/zero of=/swapfile count=4096 bs=1MiB
+  sudo chmod 600 /swapfile
+  sudo mkswap /swapfile
+  sudo swapon /swapfile
+
+  echo -e '/swapfile\t none\t swap\t sw\t 0 0' | sudo tee --append /etc/fstab > /dev/null
+}
+
+debian_swap() {
+  sudo fallocate -l 4G /swapfile \
+  && sudo chmod 600 /swapfile \
+  && sudo mkswap /swapfile \
+  && sudo swapon /swapfile
 
 echo -e '/swapfile\t none\t swap\t sw\t 0 0' | sudo tee --append /etc/fstab > /dev/null
+}
+
+
+
+echo ""
+echo "My Operating System is CentOS/Debian (please input c or d): "
+echo ""
+read os
+
+if [[ "$os" == "c" ]]; then
+  echo "Creating swap for CentOS..."
+  centos_swap
+elif [[ "$os" == "d" ]]; then
+    echo "Creating swap for Debian..."
+    debian_swap
+else "Unsupported OS. Exiting..."
+  exit 0
+fi
