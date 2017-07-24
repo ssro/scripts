9a091bf4754fe178220f34d4860247b0b11c56dd
Sebastian Sasu <sebastian.s@pocketplaylab.com>
2017-07-24 15:50:00 +0700

    Added debian section

--- docker-install.sh
+++ docker-install.sh
@@ -1,30 +1,84 @@
 #!/bin/bash
+
+# Docker automated installation for CentOS 7 or Debian Stretch/Jessie
+
+centos_docker() {
 # Docker automated installation on CentOS 7
-
 # Install epel repo, yum-utils and update system
-sudo yum -y install epel-release yum-utils \
-  && sudo yum -y update
+  sudo yum -y install epel-release yum-utils \
+    && sudo yum -y update
 
-# Remove old docker installs if any
-sudo yum -y remove docker \
-  docker-common \
-  container-selinux \
-  docker-selinux \
-  docker-engine
+    # Remove old docker installs if any
+    sudo yum -y remove docker \
+    docker-common \
+    container-selinux \
+    docker-selinux \
+    docker-engine
 
-# Add docker-ce repo to yum
-sudo yum-config-manager -y \
-  --add-repo \
-  https://download.docker.com/linux/centos/docker-ce.repo
+    # Add docker-ce repo to yum
+    sudo yum-config-manager -y \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
 
-# Update yum package index
-sudo yum -y makecache fast
+    # Update yum package index
+    sudo yum -y makecache fast
 
-# Install docker-ce
-sudo yum -y install docker-ce
+    # Install docker-ce
+    sudo yum -y install docker-ce
 
-# Add current user to docker group
-sudo usermod -aG docker $USER
+    # Add current user to docker group
+    sudo usermod -aG docker "$USER"
 
-# Enable docker at startup
-sudo systemctl enable docker
+    # Enable docker at startup
+    sudo systemctl enable docker
+}
+
+debian_docker() {
+  # Docker automated installation on debian stretch and/or jessie
+
+  # Start clean (remove old docker versions, if any)
+  sudo apt-get -y remove docker docker-engine docker.io
+
+  sudo apt-get -y update && sudo apt-get -y install \
+       apt-transport-https \
+       ca-certificates \
+       curl \
+       gnupg2 \
+       software-properties-common
+
+  curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+
+  sudo add-apt-repository \
+     "deb [arch=amd64] https://download.docker.com/linux/debian \
+     $(lsb_release -cs) \
+     stable" \
+     && sudo apt-get -y update
+
+  # Install docker
+
+  sudo apt-get -y install docker-ce
+
+  # Post install
+
+  sudo usermod -aG docker "$USER"
+  sudo systemctl enable docker || exit 0
+
+}
+
+echo ""
+echo "My Operating System is CentOS/Debian (please input c or d): "
+echo ""
+read -r os
+
+if [[ "$os" == "c" ]]; then
+  echo "Installing Docker on CentOS..."
+  centos_docker
+elif [[ "$os" == "d" ]]; then
+    echo "Installing Docker on Debian..."
+    debian_docker
+else "Unsupported OS. Exiting..."
+  exit 0
+fi
+
+echo ""
+echo "You can reboot your machine or start docker right away"
